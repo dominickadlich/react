ac15bee0fee95bfa258f391e8d5c0b36e88d4b2a
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-04-21 21:50:50 -0400

    Read 2nd Chapter of 'React Key Concepts' and created SubmitButton, edited App, and main

---
+++ react-book/src/components/SubmitButton.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+function SubmitButton() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    function handleSubmit() {
+        setIsSubmitted(true);
+    };
+    return (
+        <button onClick={handleSubmit}>
+            { isSubmitted ? 'Loading...' : 'Submit'}
+        </button>
+    );
+};
+export default SubmitButton;
