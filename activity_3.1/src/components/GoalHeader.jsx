0a20c5a649261427a21d3c12dfa011e035f5a560
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-04-23 21:35:59 -0400

    Learned how to pass props. Enhanced reusability of GoalHeader & GoalItem

---
+++ activity_3.1/src/components/GoalHeader.jsx
@@ -0,0 +1,5 @@
+export default function GoalHeader({header}) {
+    return (
+        <h1>{header}</h1>
+    );
+};
---
+++ activity_3.1/src/components/GoalItem.jsx
@@ -0,0 +1,5 @@
+export default function GoalItem({text}) {
+    return (
+        <p>{text}</p>
+    );
+};
