9f4c3253eec47652b4908e14a03a8b5cc064f8cb
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-05-05 23:50:04 -0400

    Learned how to dynamically style via ColoredText

---
+++ 6_Styling/examples/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+import ColoredText from './components/ColoredText'
+
+function App() {
+
+  return (
+    <>
+      <ColoredText />
+    </>
+  )
+}
+
+export default App
