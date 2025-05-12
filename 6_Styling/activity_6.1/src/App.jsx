631f1ff258cdc807a21ba8da1ddca5bd0ab65d7b
Dominick Adlich <139933162+d-ratch@users.noreply.github.com>
2025-05-11 23:29:58 -0400

    Reiewed key for activity 6.1

--- 6_Styling/activity_6.1/src/App.jsx
+++ 6_Styling/activity_6.1/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 
-import EmailAndPassword from './components/Email&Password'
-import ConditionalEmail from '../../../5_Lists_Conditions/activity_5.1/src/components/ConditionalEmail'
+import Form from './components/Form'
 
 function App() {
 
   return (
     <>
-      <EmailAndPassword />
+      <Form />
     </>
   )
 }
---
+++ 6_Styling/activity_6.1/src/components/Form.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+function Form() {
+    const [enteredEmail, setEnteredEmail] = useState('');
+    const [emailIsValid, setEmailIsValid] = useState('');
+
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [passwordIsValid, setPasswordIsValid] = useState('');
+
+    function handleChangeEmail(event) {
+        setEnteredEmail(event.target.value);
+    }
+
+    function handleChangePassword(event) {
+        setEnteredPassword(event.target.value);
+    }
+
+    function handleSubmitForm(event) {
+        event.preventDefault();
+
+        const emailIsValid = enteredEmail.includes('@');
+        const passwordIsValid = enteredPassword.trim().length >= 6;
+
+        setEmailIsValid(emailIsValid);
+        setPasswordIsValid(passwordIsValid);
+
+        if (!emailIsValid || !passwordIsValid) {
+            return;
+        }
+
+        // do something...
+        console.log('Inputs are valid, form submitted!')
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmitForm}>
+                <div>
+                    <label htmlFor="email" className={!emailIsValid && 'invalid'}>
+                        Your email
+                    </label>
+                    <input
+                        id="email"
+                        type="email"
+                        onChange={handleChangeEmail}
+                        className={!emailIsValid && 'invalid'}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password" className={!passwordIsValid && 'invalid'}>
+                        Your password
+                    </label>
+                    <input
+                        id="password"
+                        type="password"
+                        onChange={handleChangePassword}
+                        className={!passwordIsValid && 'invalid'}
+                    />
+                </div>
+                <button>Submit</button>
+            </form>
+        </>
+    )
+}
+
+export default Form;
