00798e18c86684b843cb75b55ed398b74fcffbfb
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-05-23 23:34:00 -0500

    Created foundation for activity 7.2

---
+++ 7_Portals_Refs/activity_7.2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+import SideDrawer from './components/SideDrawer'
+import MainNavigation from './components/MainNavigation'
+
+function App() {
+  return (
+    <>
+      <MainNavigation />
+    </>
+  )
+}
+
+export default App
