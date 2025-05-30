affd2cf8a364c960aa0e17b18e8cf76235831e9b
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-05-29 22:17:21 -0500

    MPJE

--- 8_Side_Effects/example/src/components/Error.jsx
+++ 8_Side_Effects/example/src/components/Error.jsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 
 function Error(props) {
+    const { message } = props; // Destructure to extract required properties
+
     useEffect(
         () => {
             // performing same error logging
             // in a real app, a HTTP request might be sent to some analytics API
             console.log('An error occurred!')
-            console.log(props.message);
-        },[props]);
+            console.log(message);
+            // [props] // Don't use the entire props object
+        },[message]);
 
-    return <p>{props.message}</p>
+    return <p>{message}</p>
 }
 
 export default Error;
