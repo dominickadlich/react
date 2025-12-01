5d308c07dc6f7533104338f5f4b2f3e50150d0a4
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-11-30 22:59:02 -0600

    Choosing state structure examples

--- fundamentals_revisited/src/App.tsx
+++ fundamentals_revisited/src/App.tsx
@@ -1,11 +1,11 @@
-import ShoppingCart from './2.adding_interactivity/F.UpdatingArraysInState/ShoppingCart'
+import MovingDot from './2.adding_interactivity/E.updating_objects_in_state/RedDot'
 import './App.css'
 
 
 function App() {
   return (
     <div>
-      <ShoppingCart />
+      <MovingDot />
     </div>
   )
 }
