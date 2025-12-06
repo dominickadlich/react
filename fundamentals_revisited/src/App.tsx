923e1b2a801952d364dc79f42c59d1e82fc2022e
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2025-12-06 10:23:43 -0600

    Resetting state in same position

--- fundamentals_revisited/src/App.tsx
+++ fundamentals_revisited/src/App.tsx
@@ -1,11 +1,11 @@
-import CounterApp from './3.managing_state/D.preserving_&_resetting_state/Counter'
+import Scoreboard from './3.managing_state/D.preserving_&_resetting_state/ResettingSamePos'
 import './App.css'
 
 
 function App() {
   return (
     <div>
-      <CounterApp />
+      <Scoreboard />
     </div>
   )
 }
